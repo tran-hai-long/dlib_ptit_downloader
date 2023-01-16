b9925f3b7782b540d33c2258560a3f7a9dcb2251
Tran Hai Long <119793901+tran-hai-long@users.noreply.github.com>
2023-01-16 11:28:58 +0000

    Reformat with black

--- script.py
+++ script.py
@@ -10,10 +10,10 @@ subID = re.search(r"subfolder=(.+)&doc=", url).group(1)
 docID = re.search(r"doc=(.+)&bitsid=", url).group(1)
 # loop to download the entire document
 for x in range(numPages):
-    newUrl = f'http://dlib.ptit.edu.vn/flowpaper/services/view.php?doc={docID}&format=jpg&page={x + 1}&subfolder={subID}'
-    print('Downloading page %d...' % (x + 1))
+    newUrl = f"http://dlib.ptit.edu.vn/flowpaper/services/view.php?doc={docID}&format=jpg&page={x + 1}&subfolder={subID}"
+    print("Downloading page %d..." % (x + 1))
     r = requests.get(newUrl, stream=True)
-    image = f'{x + 1}.jpg'
+    image = f"{x + 1}.jpg"
     downloaded_file = open(image, "wb")
     for chunk in r.iter_content(chunk_size=256):
         if chunk:
