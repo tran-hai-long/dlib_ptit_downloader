07d545f2eb292f42cb268b07f3e63d167ae22446
Tran Hai Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-11 19:20:59 +0000

    URL validation

--- script.py
+++ script.py
@@ -4,22 +4,25 @@ import requests
 
 # ask user to input url and number of pages
 url = input("Please input the document URL: ")
-# extract document ID and subfolder ID from pasted url
-subID = re.search(r"subfolder=(.+)&doc=", url).group(1)
-docID = re.search(r"doc=(.+)&bitsid=", url).group(1)
-# loop to download the entire document
-count = 1
-while True:
-    newUrl = f"http://dlib.ptit.edu.vn/flowpaper/services/view.php?doc={docID}&format=jpg&page={count}&subfolder={subID}"
-    request = requests.get(newUrl, stream=True)
-    if request.headers["Content-Type"] == "image/png":
-        print(f"Downloading page {count}...")
-        image = f"{count}.png"
-        downloaded_file = open(image, "wb")
-        for chunk in request.iter_content(chunk_size=256):
-            if chunk:
-                downloaded_file.write(chunk)
-    else:
-        break
-    count += 1
-print("Download complete, enjoy.")
+if url[:51] == "http://dlib.ptit.edu.vn/flowpaper/services/view.php":
+    # extract document ID and subfolder ID from pasted url
+    subID = re.search(r"subfolder=(.+)&doc=", url).group(1)
+    docID = re.search(r"doc=(.+)&bitsid=", url).group(1)
+    # loop to download the entire document
+    count = 1
+    while True:
+        newUrl = f"http://dlib.ptit.edu.vn/flowpaper/services/view.php?doc={docID}&format=jpg&page={count}&subfolder={subID}"
+        request = requests.get(newUrl, stream=True)
+        if request.headers["Content-Type"] == "image/png":
+            print(f"Downloading page {count}...")
+            image = f"{count}.png"
+            downloaded_file = open(image, "wb")
+            for chunk in request.iter_content(chunk_size=256):
+                if chunk:
+                    downloaded_file.write(chunk)
+        else:
+            break
+        count += 1
+    print("Download complete, enjoy.")
+else:
+    print("Invalid url.")
